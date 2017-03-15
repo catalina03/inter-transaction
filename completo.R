f372f53db6eb23b0a8d608993f604e4b65e06bdd
Catalina Cadena <catacadena94@gmail.com>
2017-03-15 11:55:39 -0500

    Conexion a Dropbox

--- completo.R
+++ completo.R
@@ -2,6 +2,10 @@ rm(list=ls())
 
 source("ts.r")
 
-lstsb <- read.csv2("Acciones.csv", header=TRUE)
+##Conexion a Dropbox
+#camino1="C:/Users/A79947917/Dropbox/Volatilidad (1)/MKT Quality/Trading Halts_Castro/inter-transaction"
+camino2="C:/Users/catac/Dropbox/RIntraday/Sin ceros falsos"
+  
+lstsb <- read.csv2(paste(camino2,"/Acciones.csv",sep=""), header=TRUE)
 lstsb <- data.frame(paste(lstsb[,1],"2",".csv",sep=""))
 mtable=apply(lstsb,1,ts)
